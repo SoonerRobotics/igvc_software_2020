8cfb7d52c21e511458e3b88002fd4b34d87ee93b
Preston Gray <preston@Prestons-MacBook-Pro.local>
2019-09-17 21:32:51 -0500

    Send password upon switch interrupt

--- firmware/safety/RemoteControl/RemoteControl.ino
+++ firmware/safety/RemoteControl/RemoteControl.ino
@@ -8,8 +8,9 @@
 
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <credentials.h>
 
-/* for feather32u4 
+/* for feather32u4
     #define RFM95_CS 8
     #define RFM95_RST 4
     #define RFM95_INT 7
@@ -19,9 +20,10 @@
     #define RFM95_CS 8
     #define RFM95_RST 4
     #define RFM95_INT 3
-    
+    #define STOP_PIN 12
 
-/* for shield 
+
+/* for shield
     #define RFM95_CS 10
     #define RFM95_RST 9
     #define RFM95_INT 7
@@ -33,7 +35,7 @@
     #define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
     */
 
-/* Feather m0 w/wing 
+/* Feather m0 w/wing
     #define RFM95_RST     11   // "A"
     #define RFM95_CS      10   // "B"
     #define RFM95_INT     6    // "D"
@@ -70,20 +72,26 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+volatile int16_t packetnum = 0; // packet counter, we increment per xmission
+volatile bool sendStop = false;
+
+void ISR()
+{
+  sendStop = true;
+}
+
 void setup()
 {
     pinMode(RFM95_RST, OUTPUT);
     digitalWrite(RFM95_RST, HIGH);
+    // ISR on pin 12
+    pinMode(STOP_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(STOP_PIN), ISR, FALLING);
 
     Serial.begin(115200);
-    while (!Serial)
-    {
-        delay(1);
-    }
 
     delay(100);
 
-    Serial.println("Feather LoRa TX Test!");
 
     // manual reset
     digitalWrite(RFM95_RST, LOW);
@@ -93,23 +101,16 @@ void setup()
 
     while (!rf95.init())
     {
-        Serial.println("LoRa radio init failed");
-        Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
         while (1)
             ;
     }
-    Serial.println("LoRa radio init OK!");
 
     // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
     if (!rf95.setFrequency(RF95_FREQ))
     {
-        Serial.println("setFrequency failed");
         while (1)
             ;
     }
-    Serial.print("Set Freq to: ");
-    Serial.println(RF95_FREQ);
-
     // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
     // The default transmitter power is 13dBm, using PA_BOOST.
@@ -118,48 +119,21 @@ void setup()
     rf95.setTxPower(23, false);
 }
 
-int16_t packetnum = 0; // packet counter, we increment per xmission
-
 void loop()
 {
-    delay(1000);                       // Wait 1 second between transmits, could also 'sleep' here!
-    Serial.println("Transmitting..."); // Send a message to rf95_server
+  while(sendStop)
+  {
+    delay(1000);
+    rf95.send((uint8_t *)ROBOT_PASSWORD, sizeof(ROBOT_PASSWORD));
 
-    char radiopacket[20] = "Hello World #      ";
-    itoa(packetnum++, radiopacket + 13, 10);
-    Serial.print("Sending ");
-    Serial.println(radiopacket);
-    radiopacket[19] = 0;
-
-    Serial.println("Sending...");
-    delay(10);
-    rf95.send((uint8_t *)radiopacket, 20);
-
-    Serial.println("Waiting for packet to complete...");
     delay(10);
     rf95.waitPacketSent();
     // Now wait for a reply
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
 
-    Serial.println("Waiting for reply...");
     if (rf95.waitAvailableTimeout(1000))
-    {
-        // Should be a reply message for us now
-        if (rf95.recv(buf, &len))
-        {
-            Serial.print("Got reply: ");
-            Serial.println((char *)buf);
-            Serial.print("RSSI: ");
-            Serial.println(rf95.lastRssi(), DEC);
-        }
-        else
-        {
-            Serial.println("Receive failed");
-        }
-    }
-    else
-    {
-        Serial.println("No reply, is there a listener around?");
-    }
+      if(rf95.recv(buf, &len))
+        sendStop = false;
+  }
 }
