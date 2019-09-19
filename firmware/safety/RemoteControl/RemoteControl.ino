8b6cf82821da258c5650a85de8bfbbf8ca77bcc4
Noah Zemlin <noah.zemlin@gmail.com>
2019-09-19 16:42:12 -0500

    Display changes when button pressed

--- firmware/safety/RemoteControl/RemoteControl.ino
+++ firmware/safety/RemoteControl/RemoteControl.ino
@@ -8,69 +8,30 @@
 
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <credentials.h>
-
-/* for feather32u4
-    #define RFM95_CS 8
-    #define RFM95_RST 4
-    #define RFM95_INT 7
-    */
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include "credentials.h"
 
 /* for feather m0 */
-    #define RFM95_CS 8
-    #define RFM95_RST 4
-    #define RFM95_INT 3
-    #define STOP_PIN 12
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
 
-
-/* for shield
-    #define RFM95_CS 10
-    #define RFM95_RST 9
-    #define RFM95_INT 7
-    */
-
-/* Feather 32u4 w/wing
-    #define RFM95_RST     11   // "A"
-    #define RFM95_CS      10   // "B"
-    #define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-    */
-
-/* Feather m0 w/wing
-    #define RFM95_RST     11   // "A"
-    #define RFM95_CS      10   // "B"
-    #define RFM95_INT     6    // "D"
-    */
-
-#if defined(ESP8266)
-/* for ESP w/featherwing */
-#define RFM95_CS 2   // "E"
-#define RFM95_RST 16 // "D"
-#define RFM95_INT 15 // "B"
-
-#elif defined(ESP32)
-/* ESP32 feather w/wing */
-#define RFM95_RST 27 // "A"
-#define RFM95_CS 33  // "B"
-#define RFM95_INT 12 //  next to A
-
-#elif defined(NRF52)
-/* nRF52832 feather w/wing */
-#define RFM95_RST 7  // "A"
-#define RFM95_CS 11  // "B"
-#define RFM95_INT 31 // "C"
-
-#elif defined(TEENSYDUINO)
-/* Teensy 3.x w/wing */
-#define RFM95_RST 9 // "A"
-#define RFM95_CS 10 // "B"
-#define RFM95_INT 4 // "C"
-#endif
+#define STOP_PIN 9
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(5, 6, 10, 11, 12);
 
 volatile int16_t packetnum = 0; // packet counter, we increment per xmission
 volatile bool sendStop = false;
@@ -82,6 +43,17 @@ void ISR()
 
 void setup()
 {
+      display.begin();
+    // init done
+  
+    // you can change the contrast around to adapt the display
+    // for the best viewing!
+    display.setContrast(50);
+    display.setReinitInterval(10);
+  
+    display.clearDisplay();   // clears the screen and buffer
+    display.display(); // show splashscreen
+    
     pinMode(RFM95_RST, OUTPUT);
     digitalWrite(RFM95_RST, HIGH);
     // ISR on pin 12
@@ -99,17 +71,15 @@ void setup()
     digitalWrite(RFM95_RST, HIGH);
     delay(10);
 
-    while (!rf95.init())
+    if (!rf95.init())
     {
-        while (1)
-            ;
+      delay(10);
     }
 
     // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
     if (!rf95.setFrequency(RF95_FREQ))
     {
-        while (1)
-            ;
+        delay(10);
     }
     // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
@@ -117,13 +87,23 @@ void setup()
     // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
     // you can set transmitter powers from 5 to 23 dBm:
     rf95.setTxPower(23, false);
+
 }
 
 void loop()
 {
   while(sendStop)
   {
-    delay(1000);
+    // text display tests
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(BLACK);
+    display.setCursor(0,0);
+    display.print("Sent kill!");
+    display.display();
+    
+    Serial.println("Press!");
+    //delay(1000);
     rf95.send((uint8_t *)ROBOT_PASSWORD, sizeof(ROBOT_PASSWORD));
 
     delay(10);
