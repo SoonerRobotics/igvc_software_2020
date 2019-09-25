9da68193bfb2ea26bb5a9ee8b759d4bba1175187
Kichlids <kichangsongks@gmail.com>
2019-09-24 21:40:23 -0500

    Working ros pub and serial

---
+++ igvc_ws/src/igvc_serial/src/arduino_msg_receiver/arduino_msg_receiver.ino
@@ -0,0 +1,10 @@
+
+
+#include <ros.h>
+#include "igvc_msgs.msg/serial_motion_msg"
+//#include "serial_motion_msg"
+
+
+ros::NodeHandle nh;
+
+void ledLight(const 
--- igvc_ws/src/igvc_serial/src/motion_serial.py
+++ igvc_ws/src/igvc_serial/src/motion_serial.py
@@ -3,20 +3,20 @@
 # receive serial and send signals to motors
 
 import rospy
+import serial
 from igvc_msgs.msg import serial_motion_msg
 
 def call_back(data):
     # Print data
-    rospy.loginfo("Left: %f, Right: %f", data.left_motor_speed, data.right_motor_speed)
-
+    rospy.loginfo("Left speed: %f, Right speed: %f, \n", data.left_speed, data.right_speed)
 
 def receive_serial():
-    rospy.init_node('serial_receiver_node', anonymous=True)
-    rospy.Subscriber("/igvc/motion_serial", serial_motion_msg, call_back) #Double array containing left/right motor speeds
+    rospy.init_node('serial_receiver_node', anonymous = True)
+    rospy.Subscriber("/igvc/motion_serial", serial_motion_msg, call_back)
     rospy.spin()
 
 if __name__ == '__main__':
     try:
         receive_serial()
-    except rospy.ROSInterrupException:
+    except rospy.ROSInterruptException:
         pass
