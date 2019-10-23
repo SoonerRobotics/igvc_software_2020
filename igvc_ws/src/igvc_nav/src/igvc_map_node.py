f8e5e6d418d964f90b9b725b5c4ba5c4714c3bf4
Preston Gray <preston@Prestons-MacBook-Pro.local>
2019-10-22 20:42:20 -0500

    Added visualization to map node

--- igvc_ws/src/igvc_nav/src/igvc_map_node.py
+++ igvc_ws/src/igvc_nav/src/igvc_map_node.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
 import rospy
 from matplotlib import pyplot as plot
+
+def display_map_callback(data):
+    plot.plot(data)
+    plot.show
+    print(matplotlib.get_backend())
 
 def map():
     rospy.init_node('map_node', anonymous=True)
 
-    rospy.Subscriber("/igvc_vision/map", Image, map_callback) # Don't know what Image is or should be
+    rospy.Subscriber("/igvc_vision/map", OccupancyGrid, display_map_callback)
     rospy.spin()
 
 if __name__ == '__main__':
