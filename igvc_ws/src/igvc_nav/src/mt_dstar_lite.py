73dc4d9caff0a031a674db707428c8b17f9cd915
Justin Kleiber <jkleiber8@gmail.com>
2020-01-16 21:35:04 -0600

    Finishes bulk of the D*lite stuff. Next step is to move on to the moving target basic deletion and complex deletion. Then this will need to be tested and made more robuse (out of bounds exceptions incoming)

--- igvc_ws/src/igvc_nav/src/mt_dstar_lite.py
+++ igvc_ws/src/igvc_nav/src/mt_dstar_lite.py
@@ -1,4 +1,5 @@
 
+from math import sqrt
 import numpy as np
 import heapq
 
@@ -6,7 +7,9 @@ class Node:
 
     INFINITY = 1000000000
 
-    def __init__(self, x, y):
+    def __init__(self, row, col, x, y, cost = 0):
+        self.row = row
+        self.col = col
         self.x = x
         self.y = y
 
@@ -15,6 +18,9 @@ class Node:
         self.rhs = self.INFINITY
         self.par = None
         self.key = (None, None)
+
+        # Cost (for obstacles and such)
+        self.cost = cost
 
     def set_g(self, G):
         self.G = G
@@ -28,15 +34,18 @@ class Node:
     def set_key(self, key):
         self.key = key
 
+    def set_cost(self, cost):
+        self.cost = cost
+
     def __cmp__(self, other):
-        """ Sort keys with largest being highest priority """
+        """ Sort keys with lowest priority to the top of the list"""
         # Sort by the first key
-        comp_val = -cmp(self.key[0], other.key[0])
+        comp_val = cmp(self.key[0], other.key[0])
         if comp_val != 0:
             return comp_val
 
         # If there was a tie, use the second key as a tiebreaker
-        return -cmp(self.key[1], other.key[1])
+        return cmp(self.key[1], other.key[1])
 
     def __eq__(self, other):
         return (self.x == other.x) && (self.y == other.y)
@@ -47,38 +56,42 @@ class OpenList:
 
     def __init__(self):
         """ Initialize the open list """
-        # Set up an empty max heap using list and heapq
-        self.max_heap = []
-        heapq.heapify(self.max_heap)
+        # Set up an empty min heap using list and heapq
+        self.min_heap = []
+        heapq.heapify(self.min_heap)
 
     def update(self, node):
         """ Update a node entry in the open list """
-        if node in self.max_heap:
+        if node in self.min_heap:
             self.delete(node)
             self.insert(node)
 
     def insert(self, node):
         """ Add a node to the queue """
         # Push the node to the heap
-        heapq.heappush(self.max_heap, node)
+        heapq.heappush(self.min_heap, node)
 
     def delete(self, node):
         """ Delete a node from the open list """
-        if node in self.max_heap:
-            self.max_heap.remove(node)
-            heapq.heapify(self.max_heap)
+        if node in self.min_heap:
+            self.min_heap.remove(node)
+            heapq.heapify(self.min_heap)
 
     def top_key(self):
         """ Get the top value in the heap """
-        # The top key will be the "lowest value." This is because our max heap is actually just a min-heap with a comparator that sorts in reverse
-        return min(self.max_heap).key
+        if len(self.min_heap) == 0:
+            return (INFINITY, INFINITY)
+        # The top key will be the lowest priority
+        return min(self.min_heap).key
 
     def top(self):
         """ Get the top value from the list """
-        return heapq.heappop(self.max_heap)
+        if len(self.min_heap) == 0:
+            return None
+        return heapq.heappop(self.min_heap)
 
     def contains(self, val):
-        return val in self.max_heap
+        return val in self.min_heap
 
 
 
@@ -97,9 +110,20 @@ class SearchSpace:
             for j in range(self.W):
                 self.grid[i, j] = Node(i, j)
 
-    def get_node(pos):
+    def get_node(self, pos):
         """ Gets a node at a given (row, col) position from the grid """
         return self.grid[pos[0], pos[1]]
+
+    def get_successors(self, node):
+        succ = []
+        for i in range(node.x-1, node.x+1):
+            for j in range(node.y-1, node.y+1):
+                succ.append(self.grid[i,j])
+
+        return succ
+
+    def load_search_space_from_map(self, map):
+        pass
 
 
 # Algorithm: http://idm-lab.org/bib/abstracts/papers/aamas10a.pdf
@@ -114,10 +138,10 @@ class mt_dstar_lite:
         # Create a search space
         self.search_space = SearchSpace(width, height)
 
-        # Get the node the robot is on
+        # Get the node the robot is on (row, col is the pos argument)
         self.start_node = self.search_space.get_node(robot_pos)
 
-        # Get the node the goal is on
+        # Get the node the goal is on (row, col is the pos argument)
         self.goal_node = self.search_space.get_node(goal_pos)
 
         # Set the robot's node's rhs = 0
@@ -129,12 +153,19 @@ class mt_dstar_lite:
 
     def calculate_key(self, node):
         """ Calculates a node key based on its G and RHS values, as well as the distance to the goal """
-        return (min(node.G, node.rhs) + self.h(node, self.goal_node), min(node.G, node.rhs))
+        return (min(node.G, node.rhs) + self.heuristic(node, self.goal_node), min(node.G, node.rhs))
 
 
-    def h(self, node1, node2):
+    def heuristic(self, node1, node2):
         """ Compute the distance from one node to another """
-        return ((node2.x - node1.x)**2 + (node2.y - node1.y)**2)
+        return sqrt((node2.x - node1.x)**2 + (node2.y - node1.y)**2)
+
+    def cost(self, node1, node2):
+        """ Computer actual cost incurred by moving from one node to another """
+        if node1.cost < INFINITY and node2.cost < INFINITY:
+            return heuristic(node1, node2) + node1.cost + node2.cost
+        else:
+            return INFINITY
 
 
     def update_state(self, node):
@@ -176,24 +207,45 @@ class mt_dstar_lite:
                 self.open_list.delete(u_node)
 
                 # Update successors
-                # TODO
+                for s_node in self.search_space.get_successors(u_node):
+                    if s_node != self.start_node and s_node.rhs > s_node.G + self.cost(u_node, s_node):
+                        s_node.set_rhs(s_node.G + self.cost(u_node, s_node))
+                        s_node.set_par(u_node)
+                        self.update_state(s_node)
 
-            # Otherwise TODO
+
+            # Otherwise make the node inconsistent and calculate the parent nodes of its successors
             else:
                 # Set G value to infinity
                 u_node.set_g(Node.INFINITY)
 
                 # Update successors
-                # TODO
+                for s_node in self.search_space.get_successors(u_node):
+                    if s_node != self.start_node and s_node.par == u_node:
+                        # Set the RHS to be the minimum one-step lookahead
+                        s_rhs = min([node.G + self.cost(node, s_node) for node in self.search_space.get_successors(s_node)])
+                        s_node.set_rhs(s_rhs)
 
-    def basic_deletion():
+                        if s_node.rhs >= INFINITY:
+                            s_node.set_rhs(INFINITY)
+                            s_node.set_par(None)
+                        else:
+                            # Set the parent node pointer based on the parent node that has the minimum path cost to goal from this node
+                            par_nodes = [node for node in self.search_space.get_successors(s_node)]
+                            s_par = [node.G + self.cost(node, s_node) for node in par_nodes]
+                            s_par_node_idx = s_par.index(min(s_par))
+                            s_par_node = par_nodes[s_par_node_idx]
+                            s_node.set_par(s_par_node)
+                    self.update_state(s_node)
+
+    def basic_deletion(self):
         """ """
         self.start_node.set_par(None)
 
         # Calculate the
         #self.old_start.set_rhs()
 
-    def optimized_deletion():
+    def optimized_deletion(self):
         """ """
         # Initialize deletion
         self.deleted_list = set()
@@ -202,12 +254,4 @@ class mt_dstar_lite:
     def plan(self, nodes, robot_pos, goal_pos):
         pass
 
-    # IDEA: in order to reap the benefits of MT-D*Lite, we need to preserve the old part of the map that we have not traversed
-    # To do this, we can find how much the robot has moved (row and col offset) and then shift the current grid so the robot is in the middle / bottom.
-    # Then we can populate the new empty spaces in the grid with uninitialized nodes, and then assign a cost to them. We can also update the best position
-    # as this is the moving target.
-    # This way, we keep previous local frames in the path planner independent of the SLAM algorithm. We don't need to update the entire grid of nodes each time.
-    # This is a sketchy version of SLAM because it doesn't use keyframing. So we just assume the first reading was accurate and move on.
-    def update_grid(self, robot_offset, new_goal):
-        pass
 
