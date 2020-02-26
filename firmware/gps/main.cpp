4bda286ac65c28acf924ffb32e4c5203f9c3a5f7
Kichlids <kichangsongks@gmail.com>
2020-02-25 20:46:16 -0600

    Adds GPS firmware code

--- firmware/gps/main.cpp
+++ firmware/gps/main.cpp
@@ -2,14 +2,20 @@
 #include "TinyGPSPlus.h"
 #include "ArduinoJSON.h"
 
-Serial pc(USBTX,USBRX);             // To my computer
-Serial gps_serial(PA_9, PA_10);     // To the GPS
-TinyGPSPlus GPS;                    // Gps object
+// Serial to ROS (9600)
+Serial pc(USBTX,USBRX);            
+// Serial from GPS (9600)
+Serial gps_serial(PA_9, PA_10);     
+
+TinyGPSPlus GPS;                   
 
 float_t lat;
 float_t lon;
 
 char ret[256];
+
+// Time spent sleeping every pass in millis
+float_t waitTime = 20;
 
 int main()
 {
@@ -17,26 +23,34 @@ int main()
     
     StaticJsonDocument<256> doc;
     
+    // Infinite loop that gets GPS data
     while(1) {
+        
         if (GPS.encode(gps_serial.getc())) {
             
+            // GPS has signal
             if (GPS.location.isValid()) {
                 lat = GPS.location.lat();
-                lon = GPS.location.lng(); 
+                lon = GPS.location.lng();
+                
+                doc["latitude"] = lat;
+                doc["longitude"] = lon;
+                doc["hasSignal"] = true;
             }
+            // GPS has no signal
             else {
-                lat = -1;
-                lon = -1;
+                doc["latitude"] = 1000;
+                doc["longitude"] = 1000;
+                doc["hasSignal"] = false;
             }
-
-            doc['latitude'] = lat;
-            doc['longitude'] = lon;
-                
+            
+            // Convert to JSON
             serializeJson(doc, ret);
-                
+
+            // Send via serial
             pc.printf("%s\r\n", ret);
             
-            ThisThread::sleep_for(500);
+            ThisThread::sleep_for(waitTime);
         }
     }
 }
